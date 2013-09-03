1a06b92cb71c15b7139d0f198c0c92a340c5d34f
Marcelo Henrique <bissuh@gmail.com>
2013-09-03 16:43:48 -0300

    Clap clap for the team.

---
+++ ingresse-dev/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('ingresse-devApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
