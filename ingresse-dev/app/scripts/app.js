2d23f6c74528c4b4f80443fa4503238380164464
Marcelo Henrique <bissuh@gmail.com>
2013-11-05 02:26:05 -0200

    Editing routing and url pattern to HTML5.

--- ingresse-dev/app/scripts/app.js
+++ ingresse-dev/app/scripts/app.js
@@ -4,6 +4,8 @@ angular.module('ingresse-devApp', ['ngResource'])
 
 	.config(function ($routeProvider, $locationProvider) {
 
+		$locationProvider.html5Mode(true);
+
 		$routeProvider
 			.when('/', {
 				templateUrl: 'views/main.html',
@@ -26,4 +28,14 @@ angular.module('ingresse-devApp', ['ngResource'])
 	.run(function ($rootScope, $route, $routeParams, $location) {
 		$rootScope.url = '/';
 		$rootScope.location = $location;
+
+		// $scope.$watch('locationPath', function(path) {
+	 //    $location.path(path);
+		// });
+
+		// $scope.$watch(function() {
+		// 		return $location.path();
+		// 	}, function(path) {
+		// 		$scope.locationPath = path;
+		// });
 	});
