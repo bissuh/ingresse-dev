4b0f3e07d21b964b42b1f990e8403ce34503fec9
Marcelo Henrique <bissuh@gmail.com>
2013-11-18 15:15:58 -0200

    Adding disqus service as a internal script file.

--- ingresse-dev/app/scripts/app.js
+++ ingresse-dev/app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('ingresse-devApp', ['ngResource'])
+angular.module('ingresse-devApp', ['ngResource', 'ngDisqus'])
 
 	.config(function ($disqusProvider, $routeProvider, $locationProvider, $httpProvider) {
 
